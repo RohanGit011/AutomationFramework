7df56a2afba28052dff5d163d16381d762466157
KIIT <KIIT@192.168.137.160>
2025-05-06 13:46:47 +0530

    Product, Added new POM Class

---
+++ com.AutomationFramework/src/main/java/com/comcast/crm/objectrepositoryutility/ProductPage.java
@@ -0,0 +1,18 @@
+package com.comcast.crm.objectrepositoryutility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ProductPage {
+	
+	public ProductPage(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath = "//img[@src='themes/softed/images/btnL3Add.gif']")
+	private WebElement createProductBtn;
+
+}
